52864709c782f518d0aca5f4f0ac904e4d79759a
Diogo Correia <diogo.correia@wit-software.com>
2024-12-13 00:06:32 +0000

    Homemade LinkedList

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -5,3 +5,12 @@
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
 =
 src/Main.java,9/2/924f089edb4af5ee45c8a9d1129cd28e2b368138
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+C
+java-playground.iml,1/e/1efa674a515420862cf3fc4903f7143fd84d901b
+S
+#.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8/8/88a9255124c95bdc913197c120a8d560edc59c8e
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,134 @@
+package src;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello!");
+
+        LinkedList linkedList = new LinkedList();
+        linkedList.addToList(new Node(1));
+        linkedList.addToList(new Node(2));
+        linkedList.addToList(new Node(3));
+        linkedList.removeFromIndex(0);
+        linkedList.addToIndex(new Node(99), 1);
+
+        System.out.println("ReferenceLinkedList: {}" + linkedList);
+    }
+
+    public static class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+            this.next = null;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value=" + value +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+
+    public static class LinkedList {
+        Node head;
+        int length;
+
+        public LinkedList() {
+            System.out.println("Init LinkedList!");
+            this.head = null;
+            this.length = 0;
+        }
+
+        public void addToList(Node node) {
+            System.out.println("Add to list val: " + node.value);
+            // if head is null create a new item to the list
+            if (this.head == null) {
+                this.head = node;
+                this.length++;
+                return;
+            }
+
+            Node last = this.head;
+            while (last.next != null) {
+                last = last.next;
+            }
+
+            last.next = node;
+            this.length++;
+        }
+
+        public void addToIndex(Node node, int index) {
+            System.out.println("Add to list index: " + node.value + " " + index);
+            // if head is null create a new item to the list
+            if (index > this.length) {
+                throw new IndexOutOfBoundsException("Index is out of bounds! Index: " + index + " Size: " + this.length);
+            }
+
+            int position = 0;
+            Node previousNode = null;
+            Node nextNode = null;
+            Node last = this.head;
+
+            if (index == 0) {
+                this.head = node;
+                this.head.next = last;
+                this.length++;
+            }
+
+            while (last.next != null && position <= index) {
+                position++;
+                previousNode = last;
+                nextNode = last.next;
+            }
+
+            if (previousNode == null) {
+                throw new IndexOutOfBoundsException("Invalid previousNode!");
+            }
+
+            previousNode.next = node;
+            node.next = nextNode;
+            this.length++;
+        }
+
+        public void removeFromIndex(int index) {
+            System.out.println("Removing from list, index:" + index);
+
+            if (index > this.length) {
+                throw new IndexOutOfBoundsException("Index is out of bounds! Index: " + index + " Size: " + this.length);
+            }
+
+            Node prev = null;
+            Node curr = this.head;
+            Node next = curr.next;
+
+            if (index == 0) {
+                this.head = next;
+                this.length--;
+            }
+
+            int counter = 0;
+            while (next.next != null && counter <= index) {
+                counter++;
+                prev = curr;
+                curr = next;
+                next = curr.next;
+                if (counter == index) {
+                    prev.next = next;
+                    this.length--;
+                    return;
+                }
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "LinkedList{" +
+                    "head=" + head +
+                    ", length=" + length +
+                    '}';
+        }
     }
 }
